b47b6e47bd5ba38b27123c8854a4dc3be4b635e2
Chad Shurtz <leakymemory@users.noreply.github.com>
2019-05-29 17:10:16 -0700

    Lots of refactoring (#12)
    Broke out the function that buils the dice bucket so that the unit tests don't have to know about the output format.
    
    Also added label support.

--- DiceRollerTests/DiceRollerTests.cs
+++ DiceRollerTests/DiceRollerTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 
 using DiceRollerUtils;
+using System.Collections.Generic;
 
 namespace DiceRollerTests
 {
@@ -10,39 +11,67 @@ namespace DiceRollerTests
         private readonly MockRandomNumberGenerator numberGenerator = new MockRandomNumberGenerator();
 
         [Test()]
-        public void CalculateRoll_MultipleLists()
+        public void ParseRoll_BasicValidation()
         {
             var diceRoller = new DiceRoller(numberGenerator);
-            numberGenerator.QueuedResults.Enqueue(10);
-            numberGenerator.QueuedResults.Enqueue(3);
-
-            var allRolls = diceRoller.CalculateRoll("/d20 +5, d10+3");
-
-            Assert.IsTrue(allRolls.Count == 2);
-            Assert.AreEqual("*15*  : 1d20: (*+10*), Mod: (*+5*)", allRolls[0]);
-            Assert.AreEqual("*6*  : 1d10: (*+3*), Mod: (*+3*)", allRolls[1]);
-        }
-
-        [Test()]
-        public void CalculateRoll_ComplicatedRoll()
-        {
-            var diceRoller = new DiceRoller(numberGenerator);
-            numberGenerator.QueuedResults.Enqueue(10);
-            numberGenerator.QueuedResults.Enqueue(3);
-
-            var roll = diceRoller.ParseRoll(@"/d20 + 5 -3-/d10 + 7-2");
-            Assert.AreEqual("*14*  : 1d20: (*+10*), 1d10: (*-3*), Mod: (*+5, -3, +7, -2*)", roll);
-
-            numberGenerator.QueuedResults.Enqueue(10);
-            numberGenerator.QueuedResults.Enqueue(3);
+            numberGenerator.QueuedResults.Enqueue(15);
+            numberGenerator.QueuedResults.Enqueue(5);
             numberGenerator.QueuedResults.Enqueue(2);
 
-            roll = diceRoller.ParseRoll(@"d20 -/d6-2+3+/d4");
-            Assert.AreEqual("*10*  : 1d20: (*+10*), 1d6: (*-3*), 1d4: (*+2*), Mod: (*-2, +3*)", roll);
+            var roll = diceRoller.ParseRoll("d10 +2d6 +3");
+            Assert.That(roll == "Total: *25*  :  1d10: (*+15*), 2d6: (*+5, +2*), Mod: (*+3*)");
         }
 
         [Test()]
-        public void CalculateRoll_DiceMultiplier()
+        public void ParseForLabel_VarietyOfLabels()
+        {
+            var diceRoller = new DiceRoller(numberGenerator);
+            Assert.AreEqual("Total:", diceRoller.ParseForLabel(""));
+            Assert.AreEqual("Attack:", diceRoller.ParseForLabel("Attack:"));
+            Assert.AreEqual("A long label with lots of words:", diceRoller.ParseForLabel("A long label with lots of words:"));
+            Assert.AreEqual("Damage:", diceRoller.ParseForLabel("Damage:"));
+        }
+
+        [Test()]
+        public void BuildDiceBucket_ComplicatedRoll()
+        {
+            var diceRoller = new DiceRoller(numberGenerator);
+            numberGenerator.QueuedResults.Enqueue(10);
+            numberGenerator.QueuedResults.Enqueue(3);
+
+            var bucket = diceRoller.BuildDiceBucket(@"/d20 + 5 -3-/d10 + 7-2", RollType.normalRoll, new List<int>());
+            Assert.That(bucket.Count == 3);
+            Assert.That(bucket[20].Count == 1); 
+            Assert.That(bucket[20].ToArray()[0] == "+10");
+
+            Assert.That(bucket[10].Count == 1);
+            Assert.That(bucket[10].ToArray()[0] == "-3");
+
+            Assert.That(bucket[0].Count == 4);
+            Assert.That(bucket[0].ToArray()[0] == "+5");
+            Assert.That(bucket[0].ToArray()[1] == "-3");
+            Assert.That(bucket[0].ToArray()[2] == "+7");
+            Assert.That(bucket[0].ToArray()[3] == "-2");
+        }
+
+        [Test()]
+        public void BuildDiceBucket_MultipleLists()
+        {
+            var diceRoller = new DiceRoller(numberGenerator);
+            numberGenerator.QueuedResults.Enqueue(10);
+            numberGenerator.QueuedResults.Enqueue(3);
+
+            var bucket = diceRoller.BuildDiceBucket(@"d20 +5, d10+3", RollType.normalRoll, new List<int>());
+            Assert.That(bucket.Count == 3);
+            Assert.That(bucket[20].Count == 1);
+            Assert.That(bucket[20].ToArray()[0] == "+10");
+            Assert.That(bucket[10].Count == 1);
+            Assert.That(bucket[10].ToArray()[0] == "+3");
+            Assert.That(bucket[0].Count == 2);
+        }
+
+        [Test()]
+        public void BuildDiceBucket_DiceMultiplier()
         {
             var diceRoller = new DiceRoller(numberGenerator);
             numberGenerator.QueuedResults.Enqueue(10);
@@ -51,13 +80,14 @@ namespace DiceRollerTests
             numberGenerator.QueuedResults.Enqueue(8);
             numberGenerator.QueuedResults.Enqueue(3);
 
-            var roll = diceRoller.ParseRoll(@"2d20 + 5 - 3d6");
-
-            Assert.AreEqual("*14*  : 2d20: (*+10, +15*), 3d6: (*-5, -8, -3*), Mod: (*+5*)", roll);
+            var bucket = diceRoller.BuildDiceBucket(@"2d20 + 5 - 3d6", RollType.normalRoll, new List<int>());
+            Assert.That(bucket.Count == 3);
+            Assert.That(bucket[20].Count == 2);
+            Assert.That(bucket[6].Count == 3);
         }
 
         [Test()]
-        public void CalculateRoll_LargeDiceMultiplier()
+        public void BuildDiceBucket_LargeDiceMultiplier()
         {
             var diceRoller = new DiceRoller(numberGenerator);
             numberGenerator.QueuedResults.Enqueue(15);
@@ -73,38 +103,28 @@ namespace DiceRollerTests
             numberGenerator.QueuedResults.Enqueue(15);
             numberGenerator.QueuedResults.Enqueue(15);
 
-            var roll = diceRoller.ParseRoll(@"12d20 + 5");
-
-            Assert.AreEqual("*185*  : 12d20: (*+15, +15, +15, +15, +15, +15, +15, +15, +15, +15, +15, +15*), Mod: (*+5*)", roll);
+            var bucket = diceRoller.BuildDiceBucket("12d20 + 5", RollType.normalRoll, new List<int>());
+            Assert.That(bucket.Count == 2);
+            Assert.That(bucket[20].Count == 12);
         }
 
         [Test()]
-        public void CalculateRoll_ComplicatedRollWithBadData()
+        public void BuildDiceBucket_ComplicatedRollWithBadData()
         {
             var diceRoller = new DiceRoller(numberGenerator);
             numberGenerator.QueuedResults.Enqueue(10);
             numberGenerator.QueuedResults.Enqueue(3);
             numberGenerator.QueuedResults.Enqueue(5);
 
-            var roll = diceRoller.ParseRoll(@"d20 + fred -george !r#andom %%text IS $here + 5-3-2d10 + 7-2");
-
-            Assert.AreEqual("*9*  : 1d20: (*+10*), 2d10: (*-3, -5*), Mod: (*+5, -3, +7, -2*)", roll);
+            var bucket = diceRoller.BuildDiceBucket("d20 + fred -george !r#andom %%text IS $here + 5-3-2d10 + 7-2", RollType.normalRoll, new List<int>());
+            Assert.That(bucket.Count == 3);
+            Assert.That(bucket[20].Count == 1);
+            Assert.That(bucket[10].Count == 2);
+            Assert.That(bucket[0].Count == 4);
         }
 
         [Test()]
-        public void CalculateRoll_RollWithUserSpecified()
-        {
-            var diceRoller = new DiceRoller(numberGenerator);
-            numberGenerator.QueuedResults.Enqueue(10);
-            numberGenerator.QueuedResults.Enqueue(3);
-
-            var roll = diceRoller.ParseRoll(@"1d20 +4 +/d6 @fred");
-
-            Assert.AreEqual("*17*  : 1d20: (*+10*), 1d6: (*+3*), Mod: (*+4*)", roll);
-        }
-
-        [Test()]
-        public void CalculateRoll_RollWithAdvantageMultipleD20s()
+        public void BuildDiceBucket_RollWithAdvantageMultipleD20s()
         {
             var diceRoller = new DiceRoller(numberGenerator);
             numberGenerator.QueuedResults.Enqueue(10);
@@ -116,25 +136,17 @@ namespace DiceRollerTests
             // Right now the way this is coded, each d20 will get multiple rolls, so effectively each roll
             // will be rolled with advantage.  Not sure if this is what we really want in the end or not.
 
-            var roll = diceRoller.ParseRoll(@"2d20+4 /adv");
-
-            Assert.AreEqual("*34*  : 2d20: (*+10, +20*), Mod: (*+4*), Thrown out: (*3, 6*)\n", roll);
+            var throwAway = new List<int>();
+            var bucket = diceRoller.BuildDiceBucket("2d20+4 /adv", RollType.withAdvantage, throwAway);
+            Assert.That(bucket.Count == 2);
+            Assert.That(bucket[20].Count == 2);
+            Assert.That(throwAway.Count == 2);
+            Assert.That(bucket[20].ToArray()[0] == "+10");
+            Assert.That(bucket[20].ToArray()[1] == "+20");
         }
 
         [Test()]
-        public void CalculateRoll_RollWithAdvantage()
-        {
-            var diceRoller = new DiceRoller(numberGenerator);
-            numberGenerator.QueuedResults.Enqueue(10);
-            numberGenerator.QueuedResults.Enqueue(3);
-
-            var roll = diceRoller.ParseRoll(@"d20+4 adv");
-
-            Assert.AreEqual("*14*  : 1d20: (*+10*), Mod: (*+4*), Thrown out: (*3*)\n", roll);
-        }
-
-        [Test()]
-        public void CalculateRoll_RollWithAdvantageMoreComplicatedRoll()
+        public void BuildDiceBucket_RollWithAdvantageMoreComplicatedRoll()
         {
             var diceRoller = new DiceRoller(numberGenerator);
             numberGenerator.QueuedResults.Enqueue(10);
@@ -143,21 +155,32 @@ namespace DiceRollerTests
             numberGenerator.QueuedResults.Enqueue(6);
             numberGenerator.QueuedResults.Enqueue(2);
 
-            var roll = diceRoller.ParseRoll(@"d20 +2d8 -7 +2 -1d4 adv");
+            var throwAway = new List<int>();
+            var bucket = diceRoller.BuildDiceBucket("d20 +2d8 -7 +2 -1d4 adv", RollType.withAdvantage, throwAway);
+            Assert.That(bucket.Count == 4);
 
-            Assert.AreEqual("*13*  : 1d20: (*+10*), 2d8: (*+4, +6*), 1d4: (*-2*), Mod: (*-7, +2*), Thrown out: (*3*)\n", roll);
+            Assert.That(bucket[20].Count == 1);
+            Assert.That(bucket[20].ToArray()[0] == "+10");
+            Assert.That(throwAway.Count == 1);
+
+            Assert.That(bucket[8].Count == 2);
+            Assert.That(bucket[4].Count == 1);
         }
 
         [Test()]
-        public void CalculateRoll_RollWithDisadvantage()
+        public void BuildDiceBucket_RollWithDisadvantage()
         {
             var diceRoller = new DiceRoller(numberGenerator);
             numberGenerator.QueuedResults.Enqueue(10);
             numberGenerator.QueuedResults.Enqueue(3);
 
-            var roll = diceRoller.ParseRoll(@"d20+4 /dis");
+            var throwAway = new List<int>();
+            var bucket = diceRoller.BuildDiceBucket("d20+4 /dis", RollType.withDisadvantage, throwAway);
+            Assert.That(bucket.Count == 2);
 
-            Assert.AreEqual("*7*  : 1d20: (*+3*), Mod: (*+4*), Thrown out: (*10*)\n", roll);
+            Assert.That(bucket[20].Count == 1);
+            Assert.That(bucket[20].ToArray()[0] == "+3");
+            Assert.That(throwAway.Count == 1);
         }
 
         [Test()]
